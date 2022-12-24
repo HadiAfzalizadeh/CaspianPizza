2b42a336eca454d1f476c5e7eadae0a784b32efb
HadiAfzalizadeh <hadiafzalizadeh@gmail.com>
2022-12-25 00:28:33 +0330

    formik

--- src/SharedComponents/Auth.js
+++ src/SharedComponents/Auth.js
@@ -1,25 +1,42 @@
-import { Component } from "react"
 
-export class Auth extends Component {
-    render(){
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const Auth = () => {
+	const formik = useFormik({
+		initialValues: {
+		  firstName: '',
+		  lastName: '',
+		  email: '',
+		},
+		validationSchema: Yup.object({
+		  firstName: Yup.string()
+			.max(15, 'Must be 15 characters or less')
+			.required('Required'),
+		  lastName: Yup.string()
+			.max(20, 'Must be 20 characters or less')
+			.required('Required'),
+		  email: Yup.string().email('Invalid email address').required('Required'),
+		}),
+		onSubmit: values => {
+		  alert(JSON.stringify(values, null, 2));
+		},
+	  });
         return(
             <div className="login-wrap">
 	<div className="login-html">
 		<input id="tab-1" type="radio" name="tab" className="sign-in" checked></input><label for="tab-1" className="tab">Sign In</label>
 		<input id="tab-2" type="radio" name="tab" className="sign-up"></input><label for="tab-2" className="tab">Sign Up</label>
 		<div className="login-form">
-			<div className="sign-in-htm">
+			<form className="sign-in-htm">
 				<div className="group">
-					<label for="user" className="label">Username</label>
-					<input id="user" type="text" className="input"></input>
+					<label for="email" className="label">Email Address</label>
+					<input id="email" type="text" className="input"></input>
 				</div>
 				<div className="group">
 					<label for="pass" className="label">Password</label>
 					<input id="pass" type="password" className="input" data-type="password"></input>
-				</div>
-				<div className="group">
-					<input id="check" type="checkbox" className="check" checked></input>
-					<label for="check"><span className="icon"></span> Keep me Signed in</label>
 				</div>
 				<div className="group">
 					<input type="submit" className="button" value="Sign In"></input>
@@ -32,7 +49,7 @@ export class Auth extends Component {
                     <br></br>
 					<a href="#forgot" style={{ color: 'white' , textDecoration: 'none' }}>Forgot Password?</a>
 				</div>
-			</div>
+			</form>
 			<div className="sign-up-htm">
 				<div className="group">
 					<label for="user" className="label">Username</label>
@@ -62,5 +79,6 @@ export class Auth extends Component {
 	</div>
 </div>
         )
-    }
 }
+
+export default Auth;
